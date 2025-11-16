8aa455e8a711365755dd1edb6e6bcb6cf8194338
Marcin15 <mkoperczuk1@wp.pl>
2025-11-16 11:56:13 +0100

    fix

--- Run-application.ps1
+++
@@ -1,6 +0,0 @@
-$exe = Get-ChildItem -Path $PSScriptRoot -Recurse -Filter "DbMetaTool.exe" | Select-Object -First 1
-if ($exe) {
-    & $exe.FullName
-} else {
-    Write-Host "DbMetaTool.exe not found"
-}
